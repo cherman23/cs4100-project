52f28510fe6e45a1d0767ff9031513feac117609
cherman27 <carterherman27@gmail.com>
2026-03-25 14:16:24 -0400

    fix

--- train.py
+++ train.py
@@ -3,6 +3,21 @@ from torch import optim
 from model import CNN
 
 criterion = nn.CrossEntropyLoss()
-model = CNN()
 
+model = CNN()
 optimizer = optim.SGD(model.parameters(), lr=0.001) 
+
+def train(model_name, classes, cycles):
+    for _ in cycles:
+        # Import data from data processing
+        data = {}
+        
+
+        scores = model(data)
+        loss = criterion(scores,classes)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+        nn.save(model.state_dict(),model_name + '.pth')
